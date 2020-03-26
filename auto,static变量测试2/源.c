728ad9d29e60743a0159b157d7f0979d4eee911d
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-03-26 12:19:04 +0800

    练习

---
+++ auto,static变量测试2/源.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+void AddOne()
+{
+	static int iInt2 = 10;
+	iInt2 = iInt2 + 1;
+	printf("%d\n", iInt2);
+
+}
+int main()
+{
+	printf("第五次调用：");
+	AddOne();
+	printf("第六次调用：");
+	AddOne();
+	printf("第七次调用：");
+	AddOne();
+	printf("第八次调用：");
+	AddOne();
+	printf("第九次调用：");
+	AddOne();
+	printf("第十次调用：");
+	AddOne();
+	printf("第十一次调用：");
+	AddOne();
+	printf("第十二次调用：");
+	AddOne();
+	system("pause");
+	return 0;
+}
