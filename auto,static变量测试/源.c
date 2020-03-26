326fd1867a536bd185b2aeda37a048d0e4b387b1
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-03-26 12:15:07 +0800

    测试

--- auto,static变量测试/源.c
+++ auto,static变量测试/源.c
@@ -2,10 +2,9 @@
 #include<stdlib.h>
 void AddOne()
 {
-	auto int iInt = 1;
-	iInt = iInt + 1;
-	printf("%d\n", iInt);
-
+	auto int iInt1 = 1;
+	iInt1 = iInt1 + 1;
+	printf("%d\n", iInt1);
 }
 int main()
 {
@@ -13,6 +12,11 @@ int main()
 	AddOne();
 	printf("第二次调用：");
 	AddOne();
+	printf("第三次调用：");
+	AddOne();
+	printf("第四次调用：");
+	AddOne();
 	return 0;
 	system("pause");
+
 }
