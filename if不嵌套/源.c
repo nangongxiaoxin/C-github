02ecda962364febbc3a1a47aa348a713c6013158
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-03-31 18:14:32 +0800

    if

---
+++ if不嵌套/源.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int x;
+	int y=0;
+	printf("ÇëÊäÈëxµÄÖµ:");
+	scanf_s("%d",&x);
+
+	if (-5<x&&x<0) y=x;
+	if (x==0) y=x-1;
+	if (0<x&&x<5) y=x+1;
+	printf("y=%d\n",y);
+	
+	system("pause");
+	return 0;
+}
