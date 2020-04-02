6ff0292fe1e463c8d6110b66eaa484f8150f53c5
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-04-02 08:55:50 +0800

    修改

--- if多分支语句/源.c
+++ if多分支语句/源.c
@@ -8,11 +8,16 @@ int main()
 	scanf_s("%d", &x);
 
 	if (-5 < x && x < 0) y = x;
-		else if (x == 0) y = x - 1;
-		else if (0 < x && x < 5); y = x + 1;
-		
-		printf("y=%d\n", y);
-		system("pause");
-		return 0;
+	else if (x == 0)
+	{
+		y = x - 1;
+	}
+	else if (0 < x && x < 5)
+	{
+		y = x + 1;
+	}
+	printf("y=%d\n", y);
+	system("pause");
+	return 0;
 }
 
