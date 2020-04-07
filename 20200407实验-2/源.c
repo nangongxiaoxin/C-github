d9315100c5e153c048fab60277ee6e749432a3fe
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-04-07 18:04:23 +0800

    修改

--- 20200407实验-2/源.c
+++ 20200407实验-2/源.c
@@ -25,3 +25,4 @@ int main()
 	system("pause");
 	return 0;
 }
+
---
+++ 循环累加和/源.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a = 1;
+	int sum1 = 0;
+	int b = 1;
+	int c = 0;
+	int sum2 = 0;
+	float d = 1;
+	float sum3 = 0;
+	float sum = 0;
+
+	while (a <= 100)
+	{
+		sum1 = sum1 + a;
+		a++;
+	}
+
+	do
+	{
+		sum2 = sum2+c*c;
+		c=b++;
+	} while (c <= 50);
+
+	for (; d <= 10;)
+	{
+		sum3 = sum3+1/(d++);	
+	}
+
+	sum = sum1 + sum2 + sum3;
+
+	printf("ºÍÎª£º%3.2f",sum);
+	system("pause");
+	return 0;
+}
