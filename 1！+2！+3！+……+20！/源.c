21b2b78176611aa3493b8a3cfd97e7e39006ecfc
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-04-07 18:58:19 +0800

    for-阶乘

---
+++ 1！+2！+3！+……+20！/源.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	float i = 1;
+	float a = 1;
+	float sum = 1;
+
+	for (; i <= 20; i++)
+	{
+		a =a+a * i;
+		sum = sum + a;
+
+	}
+	printf("1+2!+3!+...+20!=%f",sum);
+	system("pause");
+	return 0;
+}
