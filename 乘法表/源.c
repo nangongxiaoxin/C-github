78cd7506fc744357c4f402241722a691b395b3b2
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2020-04-10 17:16:50 +0800

    九九乘法表

---
+++ 乘法表/源.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b;
+	for (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%2d  ", b,a , a * b);
+		}
+
+		printf("\n");
+	}
+
+
+
+	system("pause");
+	return 0;
+}
