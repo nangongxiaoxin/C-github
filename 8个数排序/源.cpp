8ad2e577896f05183b2c7cdfa97d12724cd41a13
nangongxiaoxin <1764759472@qq.com>
2020-05-23 21:18:48 +0800

    实验七

---
+++ 8个数排序/源.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>                              //从大到小排序
+int main()
+{
+    int x[8];
+    int a, b, c;
+    printf("输入8个需要排序的数：\n");
+    for (b = 0; b < 8; b++)
+    {
+        scanf_s("%d", &x[b]);
+    }
+    printf("\n");
+    for (a = 0; a < 7; a++)
+    {
+        for (b = 0; b < 7 - a; b++)
+        {
+            if (x[b] < x[b + 1])                 //比较
+            {
+                c = x[b];                        //传递
+                x[b] = x[b + 1];
+                x[b + 1] = c;
+            }
+        }
+    }
+    printf("\n");
+    for (b = 0; b < 8; b++)
+    {
+        printf(" %d", x[b]);
+    }
+    system("pause");
+    return 0;
+}
