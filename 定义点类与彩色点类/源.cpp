c7ac391d827c45c1d83b30130199d33ba1a09813
许涛 <1764759472@qq.com>
2020-09-27 21:30:15 +0800

    0927

--- 定义点类与彩色点类/源.cpp
+++ 定义点类与彩色点类/源.cpp
@@ -1,28 +1,50 @@
 #include<iostream>
+#include<string>
 using namespace std;
 class point
 {
 private:
-	int x = 1, y = 2, z = 3;
+	int x, y, z;
 public:
-	void A(int x, int y, int z)
+	int a, b, c;
+	void A(int b1, int b2, int b3)
 	{
-		//this->x = x;
-		//this->y = y;
-		//this->z = z;	//this指针可以用私有变量下的x,y,z对此处的x,y,z进行重新赋值
-		cout << x << " " << y << " " << z << endl;
+		x = b1;
+		y = b2;
+		z = b3;	//this指针可以用私有变量下的x,y,z对此处的x,y,z进行重新赋值
+		//cout << x << " " << y << " " << z << endl;
 	}
 	void show()
 	{
-		cout << x << " " << y << " " << z << endl;
+		cout << x << ' ' << y << ' ' << z << endl;
+	}
+};
+class colorpt:public point
+{
+private:
+	int a1, a2, a3;
+	char a[20];
+public:
+	void B(int a1, int a2, int a3, const char b[20] = {})
+	{
+		this->a1 = a1;
+		this->a2 = a2;
+		this->a3 = a3;
+		strcpy_s(a,b);
+	}
+	void show()
+	{
+		cout << a1 << ' ' << a2 << ' ' << a3 << ' ' << a << endl;
 	}
 };
 int main()
 {
 	point q;
-	q.A(4, 5, 6);
-	q.A(7, 8, 9);	//模块化操作，将功能与数据分开
+	q.A(2, 3, 5);
 	q.show();
+	colorpt w;
+	w.B(10, 20, 30,"red");
+	w.show();
 	system("pause");
 	return 0;
 }
